80576f5f1ade00d3464066b6a6e6397ef64e23c1
42nvtn <42nvtn@gmail.com>
2022-11-12 07:45:28 +0700

    Update TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,35 @@
 import React, {useState} from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
+  const [input, setInput] = useState('');
+
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // props.onSubmit({
+    //   id: Math.floor(Math.random() * 10000),
+    //   text: input
+    // });
+    setInput('');
+  };
+
   return (
-    <div>TodoForm</div>
-  )
+    <form className='todo-form' onSubmit={handleSubmit}>
+      <input 
+      type='text'
+      placeholder='Add a todo'
+      value={input}
+      name='text'
+      className='todo-input'
+      onChange={handleChange}
+      />
+      <button className='todo-button'>Add a todo</button>
+    </form>
+  );
 }
 
-export default TodoForm
+export default TodoForm;
